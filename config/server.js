933fef081e16ce73dbb2bcad2d86be22cc60302a
Alexis Guzman <alexis12989x@gmail.com>
2021-02-08 18:27:47 -0600

    Set up strapi with SQLite

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7e18bc48962bdfeefd866341df55c0d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28892f0e-eaac-40c9-951d-fc4d8d50c6de'
+};
